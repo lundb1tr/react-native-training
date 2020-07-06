db267b7b88f319f328aa8b1fdf3298d88af13f69
Tyler Lundberg <35784267+lundb1tr@users.noreply.github.com>
2020-07-06 16:33:57 -0400

    initial commit

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+const ListScreen = () => {
+  const friends = [
+    { name: "Heidi", age: 30 },
+    { name: "Patrick", age: 35 },
+    { name: "Ericka", age: 29 },
+    { name: "Andy", age: 32 },
+    { name: "Carl", age: 34 },
+    { name: "Van", age: 36 },
+    { name: "Teilia", age: 30 },
+    { name: "Michael", age: 28 },
+    { name: "Austin", age: 27 },
+  ];
+  return (
+    <FlatList
+      showsVerticalScrollIndicator={false}
+      keyExtractor={(friend) => friend.name}
+      data={friends}
+      renderItem={({ item }) => {
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} - Age: {item.age}
+          </Text>
+        );
+      }}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50,
+  },
+});
+
+export default ListScreen;
