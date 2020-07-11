fd9d4ce5fd6917430a4eceda61f97d5c4e5cfb2c
Tyler Lundberg <35784267+lundb1tr@users.noreply.github.com>
2020-07-10 23:39:38 -0400

    passing more variables as props

--- src/Components/ImageDetail.js
+++ src/Components/ImageDetail.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = ({ title }) => {
+const ImageDetail = ({ title, imageSource, score }) => {
   return (
     <View>
+      <Image source={imageSource}/>
       <Text>{title}</Text>
+      <Text>{score}</Text>
     </View>
   );
 };
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,10 +5,15 @@ import ImageDetail from '../Components/ImageDetail';
 const ImageScreen = () => {
 	return (
 		<View>
-			<ImageDetail title="Forest"/>
-			<ImageDetail title="Beach"/>
-			<ImageDetail title="Desert"/>
-			<ImageDetail title="Mountain"/>
+			<ImageDetail title="Forest"
+				imageSource={require('../../assets/forest.jpg')}
+				score="Image Score - 8/10"/>
+			<ImageDetail title="Beach"
+				imageSource={require('../../assets/beach.jpg')}
+				score="Image Score - 3/10"/>
+			<ImageDetail title="Mountain"
+				imageSource={require('../../assets/mountain.jpg')}
+				score="Image Score - 10/10"/>
 		</View>
 	)
 }
