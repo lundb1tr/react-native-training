b4d724f3c980051a172250f876940b5f3b3919fe
Tyler Lundberg <35784267+lundb1tr@users.noreply.github.com>
2020-07-14 18:46:28 -0400

    some text verification

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
+  const [password, setPassword] = useState('');
   return (
     <View>
+      <Text>Enter Password:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
-        placeholder="Tim Sucks"
+        placeholder="password"
+        value={password}
+        onChangeText={newValue => setPassword(newValue)}
       />
+      {password.length < 4 ? (
+        <Text>Password must be at least four characters</Text>
+      ) : null}
     </View>
   );
 };
