b3811263adfa49e44b49c20ef4385ec5afd149ae
Tyler Lundberg <35784267+lundb1tr@users.noreply.github.com>
2020-07-14 19:33:55 -0400

    flex items and position

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,7 +4,9 @@ import { Text, View, StyleSheet } from 'react-native';
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Box Screen</Text>
+      <Text style={styles.textOneStyle}>Box Screen</Text>
+      <Text style={styles.textTwoStyle}>Child 2</Text>
+      <Text style={styles.textThreeStyle}>Child 3</Text>
     </View>
   );
 };
@@ -13,11 +15,21 @@ const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 3,
     borderColor: 'black',
+    height: 200,
   },
-  textStyle: {
-    borderWidth: 10,
+  textOneStyle: {
+    borderWidth: 3,
     borderColor: 'red',
-    margin: 20,
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+    position: 'absolute',
+    fontSize: 18,
+  },
+  textThreeStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
   },
 });
 
