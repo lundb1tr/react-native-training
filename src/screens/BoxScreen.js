d13b5a9becd4a3694eb3a22b1cb3a7c7359d7271
Tyler Lundberg <35784267+lundb1tr@users.noreply.github.com>
2020-07-14 19:50:37 -0400

    my exercise solution

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,33 +3,48 @@ import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textOneStyle}>Box Screen</Text>
-      <Text style={styles.textTwoStyle}>Child 2</Text>
-      <Text style={styles.textThreeStyle}>Child 3</Text>
+    <View style={styles.parentStyle}>
+      <View style={styles.viewOneStyle}></View>
+      <View style={styles.viewTwoStyle}></View>
+      <View style={styles.viewThreeStyle}></View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  viewStyle: {
-    borderWidth: 3,
-    borderColor: 'black',
+  parentStyle: {
     height: 200,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
-  textOneStyle: {
+  viewOneStyle: {
     borderWidth: 3,
     borderColor: 'red',
+    backgroundColor: 'red',
+    opacity: 80,
+    height: 80,
+    width: 80,
+    borderRadius: 5,
   },
-  textTwoStyle: {
+  viewTwoStyle: {
     borderWidth: 3,
-    borderColor: 'red',
+    borderColor: 'green',
+    backgroundColor: 'green',
+    opacity: 80,
     fontSize: 18,
-    ...StyleSheet.absoluteFillObject,
+    borderRadius: 5,
+    height: 80,
+    width: 80,
+    top: 80,
   },
-  textThreeStyle: {
+  viewThreeStyle: {
     borderWidth: 3,
-    borderColor: 'red',
+    borderColor: 'purple',
+    backgroundColor: 'purple',
+    opacity: 80,
+    height: 80,
+    width: 80,
+    borderRadius: 5,
   },
 });
 
